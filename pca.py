1cd2d6ece3077885c15bd81d2df485053dd63eac
Nura Kawa <nkawa@berkeley.edu>
2019-02-21 14:01:40 -0800

    re-wrote as sklearn estimator

--- pca.py
+++ pca.py
@@ -1,108 +1,140 @@
 # =============================================================================
 # title: 			pca.py
 # author: 			Nura Kawa
-# date: 			20 March 2017
-# summary:			implements principal component analysis in python
-#					from singular value decomposition
-# data:				subset of mnist dataset
+# summary:			Principal component analysis as a sklearn estimator
+#					Uses singular value decomposition from numpy.linalg
+#					tested with the iris dataset from sklearn
 # =============================================================================
 
 # Import Libraries
-import scipy as sp
-import pandas as pd
+# -----------------------------------------------------------------------------
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn import datasets
 import numpy as np
+import math
 import numpy.linalg as la
 import matplotlib.pyplot as plt
-import math
 
-# Set Plot Features
-plt.style.use('ggplot')
+# PCA
+# -----------------------------------------------------------------------------
+"""
+class PCA
 
-def main(scaled):
+	inherits:
+				class BaseEstimator from sklearn
+				class TransformerMixin from sklearn
 
-	# read in data
-	df = pd.read_csv('../Desktop/spring-2017/r-workshop/mnist_test.csv')
-	label = df.iloc[:,0] # save the label
-	del df['0'] # deletes the label column - we do not want to perform PCA with it
-	
-	# optional: scale your data
-	if scaled == True:
-		df = Scale(df)
-	
-	# make the components
-	svd_item = Princomp(df)
-	
-	# keep the eigenvalues and eigenvectors
-	eigenvalues = svd_item[1]
-	eigenvectors = svd_item[0]
-	
-	# compute the principal components
-	columns = []
-	for i in range(len(df.columns)):
-		columns.append("Component" + str(i+1))
-	
-	principal_components = pd.DataFrame(index = range(len(df.index)),columns = columns)
-		
-	for i in range(len(principal_components.columns)):
-		principal_components.iloc[:,i] = eigenvalues[i] * eigenvectors[i] 
-	
-	# plot the PCA scores
-	PCA_Scores_Plot(svd_item, label)
-	
-	# plot the screeplot
-	Screeplot(svd_item)
-	
-	# plot the loadings
-	PCA_Plot(principal_components)	
-	
-def Scale(df):
-	# df should be a pandas DataFrame
-	for i in range(len(df.columns)):
-	#for i in range(ncol):
-		col = df.iloc[:,i]
-		scaled_col = (col - np.mean(col)) / np.std(col)
-		df.iloc[:,i] = scaled_col
-	return df
+	attributes:
+				scaled (scale the data?)
+				scores (PCA scores)
+				components (Matrix of Principal Components)
+				loadings (Array of loadings)
 
-def Princomp(data):
-	# data should be pandas dataframe
-	mat = pd.DataFrame.as_matrix(data)
-	return la.svd(mat)
-		
-def Screeplot(princomp_item):
-	eigs = princomp_item[1] # the singular values
-	sorted_eigs = sorted(eigs, reverse = True)
-	sorted_eigs = sorted_eigs[0:10] # only need to keep 10 components
-	plt.plot(sorted_eigs, marker = 'o')
-	plt.xlabel('Number of Components')
-	plt.ylabel('Eigenvalues')
-	plt.title('Screeplot')
-	plt.grid(True)
-	plt.show()
+	new methods:
+				to see all methods run `dir(PCA())`
 
-def PCA_Plot(component_matrix):
-	comp_1 = component_matrix["Component1"]
-	comp_2 = component_matrix["Component2"]
-	plt.scatter(comp_1, comp_2)
-	plt.grid(True)
-	plt.title('PCA Loadings')
-	plt.xlabel('Component1')
-	plt.ylabel('Component2')
-	plt.show()
+				__init__ 	follows rules for sklearn estimator
+				fit			performs pca on dataset
+				_scale		scales data
+				plot		plots PCA scores
 
-def PCA_Scores_Plot(svd_item, label):
-	scores_1 = svd_item[0][:,0]
-	scores_2 = svd_item[0][:,1]
-	
-	plt.scatter(scores_1, scores_2, c = label)
-	
-	plt.title('PCA Scores')
-	plt.xlabel('Component1')
-	plt.ylabel('Component2')
-	plt.show()
+"""
 
-	
-if __name__ == "__main__":
-    main(False)
-	
-	
+
+
+class PCA(BaseEstimator, TransformerMixin):
+
+	def __init__(self,
+	scaled = None,
+	scores = None,
+	components = None,
+	loadings = None):
+		self.scaled = scaled
+		self.scores = scores
+		self.compoments = components
+		self.loadings = loadings
+
+	def fit(self, X, scaled = False):
+
+		# assert statements
+
+		assert isinstance(X, np.ndarray), "Please convert dataset to type 'numpy.ndarray'"
+		assert scaled in [True, False, None], "`scaled` must be True or False"
+
+		# optional: scale data
+		if scaled == True: X = self._scale(X)
+
+		# perform singular value decomposition
+		# sometimes SVD does not converge
+
+		try:
+			svd_item = la.svd(np.matrix(X, dtype='float'))
+		except:
+			print("Failed to converge. Try changing your value for `scale`")
+			return
+
+		evectors, evalues = svd_item[0], svd_item[1]
+
+		# matrix of principal components
+		principal_components = evectors[:,0:len(evalues)]
+
+		for i in range(len(evalues)):
+			principal_components[:,i] *= evalues[i]
+
+		# scaled
+		self.scaled = scaled
+		# components
+		self.components = principal_components
+		# scores
+		self.scores = svd_item[0][:,0:2]
+		# loadings
+		self.loadings = evalues
+
+		return self
+
+	def _scale(self, df):
+		scaled_df = df
+		for i in range(df.shape[1]):
+			col = df[:,i]
+			scaled_col = (col - np.mean(col)) / np.std(col)
+			scaled_df[:,i] = scaled_col
+		return scaled_df
+
+	def plot(PCA_fitted_object,
+	label = None,
+	title = None):
+		plt.scatter([PCA_fitted_object.scores[:,0]],
+		[PCA_fitted_object.scores[:,1]],
+		c = label,
+		alpha = 0.3)
+		plt.title(title)
+		plt.xlabel("Component 1")
+		plt.ylabel("Component 2")
+		plt.show()
+		return
+
+# Use with Iris Dataset
+# -----------------------------------------------------------------------------
+
+iris = datasets.load_iris()
+X = iris.data
+
+# Create and fit a PCA object
+iris_pca = PCA().fit(X, scaled = True)
+iris_pca.get_params() # this is inherited from class BaseEstimator
+
+# Plot the PCA scores
+def color_map(arr):
+	map_ = {0:'red',1:'blue',2:'green'}
+	colors=arr
+	for i in range(len(arr)):
+		colors[i] = map_[arr[i]]
+	return(colors)
+
+iris_pca.plot(label = color_map(iris.target.tolist()),
+title = "PCA View of Iris Dataset")
+
+# resources
+# -----------------------------------------------------------------------------
+# create an estimator with sklearn
+# http://danielhnyk.cz/creating-your-own-estimator-scikit-learn/
